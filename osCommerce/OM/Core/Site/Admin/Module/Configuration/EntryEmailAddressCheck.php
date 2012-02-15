bef18b5ddbdbf85ded25bc374865c7cdfed5b480
Harald Ponce de Leon <hpdl@oscommerce.com>
2012-02-15 19:49:04 +0100

    Placeholder classes for Configuration Modules

---
+++ osCommerce/OM/Core/Site/Admin/Module/Configuration/EntryEmailAddressCheck.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * osCommerce Online Merchant
+ * 
+ * @copyright Copyright (c) 2012 osCommerce; http://www.oscommerce.com
+ * @license BSD License; http://www.oscommerce.com/bsdlicense.txt
+ */
+
+  namespace osCommerce\OM\Core\Site\Admin\Module\Configuration;
+
+  use osCommerce\OM\Core\HTML;
+  use osCommerce\OM\Core\OSCOM;
+
+  class EntryEmailAddressCheck extends \osCommerce\OM\Core\Site\Admin\ConfigurationModule {
+    public function get() {
+      return parent::get();
+    }
+
+    public function getField() {
+      return parent::getField();
+    }
+  }
+?>
