880278367de36355e350ac23d2a3f27dfa3692dc
Harald Ponce de Leon <hpdl@oscommerce.com>
2012-02-16 01:11:27 +0100

    AccountCompany Configuration Module

--- osCommerce/OM/Core/Site/Admin/Module/Configuration/AccountCompany.php
+++ osCommerce/OM/Core/Site/Admin/Module/Configuration/AccountCompany.php
@@ -12,12 +12,63 @@
   use osCommerce\OM\Core\OSCOM;
 
   class AccountCompany extends \osCommerce\OM\Core\Site\Admin\ConfigurationModule {
+    protected $_param_disabled;
+    protected $_param_not_required;
+
+    public function __construct($key, $module = null) {
+      parent::__construct($key, $module);
+
+      $this->_param_disabled = OSCOM::getDef('parameter_disabled');
+      $this->_param_not_required = OSCOM::getDef('parameter_not_required');
+    }
+
     public function get() {
-      return parent::get();
+      switch ( $this->getRaw() ) {
+        case '-1':
+          return $this->_param_disabled;
+
+        case '0':
+          return $this->_param_not_required;
+
+        default:
+          return $this->getRaw();
+      }
     }
 
     public function getField() {
-      return parent::getField();
+      $field = '<label for="cfg' . $this->_module . '">' . $this->getTitle() . '</label>' . HTML::inputField('configuration[' . $this->_key . ']', $this->getRaw(), 'id="cfg' . $this->_module . '"');
+
+      $field .= <<<EOT
+<div class="sliderContainer">
+  <div class="sliderValue" id="sliderValue{$this->_module}">{$this->get()}</div>
+  <div id="slider{$this->_module}" style="width: 92%;"></div>
+</div>
+<script>
+$(function() {
+  $('#cfg{$this->_module}').hide();
+
+  $('#slider{$this->_module}').slider({
+    range: 'min',
+    value: $('#cfg{$this->_module}').val(),
+    min: -1,
+    max: 10,
+    slide: function(event, ui) {
+      $('#cfg{$this->_module}').val(ui.value);
+
+      if ( ui.value == '-1' ) {
+        $('#sliderValue{$this->_module}').html('{$this->_param_disabled}');
+      } else if ( ui.value == '0' ) {
+        $('#sliderValue{$this->_module}').html('{$this->_param_not_required}');
+      } else {
+        $('#sliderValue{$this->_module}').html(ui.value);
+      }
+    }
+  });
+});
+</script>
+EOT;
+
+      return $field;
     }
   }
 ?>
