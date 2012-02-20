7ea7a73bf1170070d3e0053305eb81a4593762ff
Harald Ponce de Leon <hpdl@oscommerce.com>
2012-02-20 16:43:32 +0100

    New Configuration Modules

---
+++ osCommerce/OM/Core/Site/Admin/Module/Configuration/BoxBestSellersCache.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * osCommerce Online Merchant
+ * 
+ * @copyright Copyright (c) 2012 osCommerce; http://www.oscommerce.com
+ * @license BSD License; http://www.oscommerce.com/bsdlicense.txt
+ */
+
+  namespace osCommerce\OM\Core\Site\Admin\Module\Configuration;
+
+/**
+ * @since v3.0.4
+ */
+
+  class BoxBestSellersCache extends \osCommerce\OM\Core\Site\Admin\Module\ConfigurationAbstract {
+    static protected $_sort = 0;
+    static protected $_default = '60';
+    static protected $_group_id = 6;
+
+    public function initialize() { }
+  }
+?>
---
+++ osCommerce/OM/Core/Site/Admin/Module/Configuration/BoxBestSellersMaxList.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * osCommerce Online Merchant
+ * 
+ * @copyright Copyright (c) 2012 osCommerce; http://www.oscommerce.com
+ * @license BSD License; http://www.oscommerce.com/bsdlicense.txt
+ */
+
+  namespace osCommerce\OM\Core\Site\Admin\Module\Configuration;
+
+/**
+ * @since v3.0.4
+ */
+
+  class BoxBestSellersMaxList extends \osCommerce\OM\Core\Site\Admin\Module\ConfigurationAbstract {
+    static protected $_sort = 0;
+    static protected $_default = '10';
+    static protected $_group_id = 6;
+
+    public function initialize() { }
+  }
+?>
